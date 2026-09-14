9bbe782975f794d590826c9a71dad1329fb2c737
nanazhii71-max <nanazhii71@gmail.com>
2026-09-14 14:54:08 +0800

    chore: initial repo scaffold (build env AGP 9.1.1/Gradle 9.3.1/NDK 28.2.13676358), archive Phase-3 CI from previous Molina-X Manager project

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+}
+
+// Dikonsumsi oleh setiap modul Android (app/, feature-*) mulai Phase 1.
+extra["molinaxNdkVersion"] = "28.2.13676358"
+extra["molinaxCompileSdk"] = 37
+extra["molinaxMinSdk"] = 26
+extra["molinaxTargetSdk"] = 26
+extra["molinaxJvmToolchain"] = 21
+extra["molinaxComposeBom"] = "2026.08.00"
